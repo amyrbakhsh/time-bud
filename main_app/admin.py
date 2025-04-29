83c0adc6cc9e1b2ae8646d42e661197d4c8a70bf
hamdan.ga <hamdanchitari@gmail.com>
2025-04-29 19:08:53 +0300

    admin&url.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Watch, Bid, Transaction, Tag
 # Register your models here.
+admin.site.register(Watch)
+admin.site.register(Bid)
+admin.site.register(Transaction)
+admin.site.register(Tag)
+
--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,21 @@ from django.urls import path
 from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home.as_views(), name='home'),
+
+    path('accounts/signup/', views.signup, name='signup'),
+
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    path('profile/<int:user_id>/', views.profile, name='profile'),
+
+    path('watches/<int:pk>/', views.watch_detail, name='watch_detail'),
+
+    path('watches/create/', views.WatchCreate.as_view(), name='watch_create'),
+
+    path('watches/<int:pk>/update/', views.WatchUpdate.as_view(), name='watch_update'),
+    
+    path('watches/<int:pk>/delete/', views.WatchDelete.as_view(), name='watch_delete'),
 ]
+
+
