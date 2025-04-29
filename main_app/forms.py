691f800c345ec1771c405d470d65763dd5f3559a
amyrbakhsh <amyr.bakhsh@gmail.com>
2025-04-29 19:03:37 +0300

    updated forms and views

---
+++ main_app/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Watch, Bid
+
+#Watch Form
+class WatchForm(forms.ModelForm):
+    class Meta:
+        model = Watch
+        fields = ['title', 'brand', 'description', 'condition', 'image', 'tags']
+
+#Bid Form
+class BidForm(forms.ModelForm):
+    class Meta:
+        model = Bid
+        fields = ['amount']
